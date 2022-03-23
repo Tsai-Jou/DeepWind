6a434b4b4284059637a9989f74b0d3a245a06bb4
Tsai-Jou <queto9612@gmail.com>
2022-03-23 19:07:44 +0800

    Create close.py

---
+++ close.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 18 15:50:47 2019
+
+@author: user
+"""
+
+import sys
+
+a = int(input("輸入數字a:"))
+b = int(input("輸入數字b:"))
+
+if a == b : 
+   sys.exit()
+
+print(a + b)
+
+int(input("輸入數字c:"))
